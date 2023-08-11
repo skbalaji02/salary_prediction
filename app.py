7be8ef165a6d128df92f860cf54e5d130389a916
skbalaji02 <109463598+skbalaji02@users.noreply.github.com>
2023-08-11 12:21:24 +0530

    app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import plotly as plo
 from plotly import graph_objs as go
-from plotly import offline as po
 from sklearn.linear_model import LinearRegression
 import numpy as np
 import time
